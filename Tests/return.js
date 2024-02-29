e3cdfbea13f13c4497602355e40f4944c2cd281b
AlexisB78 <a.beller@urmet.fr>
2024-02-29 11:21:11 +0100

    tests

---
+++ Tests/return.js
@@ -0,0 +1,8 @@
+function blabla(){
+    let res = []
+    res.push(1)
+    return res
+}
+
+let req=blabla()
+console.log(req)
---
+++ Tests/tab.js
@@ -0,0 +1,9 @@
+let obj=  [  ]
+
+
+console.log(obj)
+console.log(obj.length)
+
+const clothing = ['shoes', 'shirts', 'socks', 'sweaters'];
+console.log(clothing)
+console.log(clothing.length);
